a1d8b9bab019f6a5e609a79c718e09bb8575160e
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-29 04:49:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,10 @@
-class Song 
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist
+    @genre = genre
+  end 
+  
 end 
